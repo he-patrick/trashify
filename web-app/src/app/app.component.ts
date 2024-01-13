62a35ff9080d5474dec1e8d2fac7101a8986a6ea
edwardwang4340 <eddywang530@gmail.com>
2024-01-13 17:30:53 -0500

    test2 changes

--- web-app/src/app/app.component.ts
+++ web-app/src/app/app.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {WebcamComponent} from './webcam/webcam.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
-  templateUrl: './app.component.html',
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, WebcamComponent],
+  template: `
+  <div>
+    <webcam
+      [trigger]="trigger"
+      [width]="640"
+      [height]="480"
+      [videoOptions]="videoOptions"
+      (imageCapture)="handleImage($event)">
+    </webcam>
+  </div>
+  
+  <button (click)="triggerSnapshot()">Take Picture</button>
+  `,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
