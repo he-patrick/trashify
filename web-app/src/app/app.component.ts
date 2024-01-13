18eff429da19e30c8411fe0cba43d690ba7cb17e
edwardwang4340 <eddywang530@gmail.com>
2024-01-13 17:08:31 -0500

    tried changing routing

--- web-app/src/app/app.component.ts
+++ web-app/src/app/app.component.ts
@@ -5,7 +5,7 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- web-app/src/app/app.routes.ts
+++ web-app/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import {WebcamComponent} from './webcam/webcam.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'webcam-component', component: WebcamComponent},
+];
